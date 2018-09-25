9b25d71ff40943bca0e11dc5698cb332315f5181
Fredrik <fredrik@testi.laakariliitto.fi>
2018-09-25 18:06:53 +0300

    Small layout changes

--- application/poll/forms.py
+++ application/poll/forms.py
@@ -1,10 +1,14 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, DateTimeField, BooleanField, validators
+from wtforms import StringField, DateTimeField, BooleanField, IntegerField, validators
 
 class PollForm(FlaskForm):
     name = StringField(u"Name", [validators.Length(min=1, max=256)])
-    start = DateTimeField(u"start", [validators.Required()])
-    end = DateTimeField(u"End", [validators.Required()])    
+    start_date = DateTimeField(u"Start date", [validators.Required()])
+    start_hour = IntegerField(u"Start hour", [validators.Required(), validators.NumberRange(min=1, max=23)])
+    start_minute = IntegerField(u"Start minute", [validators.Required(), validators.NumberRange(min=0, max=59)])
+    end_date = DateTimeField(u"End", [validators.Required()])    
+    end_hour = IntegerField(u"End hour", [validators.Required(), validators.NumberRange(min=1, max=23)])
+    end_minute = IntegerField(u"End minute", [validators.Required(), validators.NumberRange(min=0, max=59)])
     delete = BooleanField(u"Delete")
 
     class Meta:
