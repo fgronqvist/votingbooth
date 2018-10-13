71d2f19420ffa15885f5ce8527418bebf8cc60f9
Fredrik Grönqvist <fredrik.gronqvist@gmail.com>
2018-10-14 02:19:09 +0300

    Layout fixes and minor logic changes

--- application/views.py
+++ application/views.py
@@ -1,11 +1,18 @@
-from flask import render_template
+from flask import render_template, redirect, url_for
+from flask_login import current_user
 from application import app
 from application.account.forms import LoginForm
 
 @app.route("/")
 def index():
+    if current_user.is_authenticated:
+        return redirect(url_for("account_index"))
     form = LoginForm()
     return render_template("index.html", form=form)
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
 
 # a simple layout to test css problems etc
 @app.route("/layoutest")
